e5d8243db21bddf45da267e60157ceb12fbc2e07
Vinícius Cardoso <viniciusgcardoso007@gmail.com>
2023-12-11 17:29:07 -0300

    Backend added and project organized

---
+++ backend/src/main/java/br/com/newterp/Newt/main/NewtErpApplication.java
@@ -0,0 +1,11 @@
+package br.com.newterp.Newt.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewtErpApplication {
+
+	public static void main(String[] args) { SpringApplication.run(NewtErpApplication.class, args); }
+
+}
---
+++ backend/src/test/java/br/com/newterp/Newt/ERP/NewtErpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.newterp.Newt.ERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewtErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
