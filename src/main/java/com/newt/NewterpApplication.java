7466c4f02521a90701ffdb72486004196e72144f
Vinícius Cardoso <viniciusgcardoso007@gmail.com>
2024-01-19 17:02:18 -0300

    SpringBoot Initialized

---
+++ src/main/java/com/newt/NewterpApplication.java
@@ -0,0 +1,13 @@
+package com.newt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewterpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewterpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newt/NewterpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewterpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
