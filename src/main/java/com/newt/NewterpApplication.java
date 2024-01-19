8483552d8380f63a425623da06a7acdaa99befdf
Vinícius Cardoso <viniciusgcardoso007@gmail.com>
2024-01-19 19:03:54 -0300

    Setting Up home page

--- src/main/java/com/newt/NewterpApplication.java
+++ src/main/java/com/newt/NewterpApplication.java
@@ -2,12 +2,20 @@ package com.newt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NewterpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewterpApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String homepage(){
+		return "index";
 	}
 
 }
